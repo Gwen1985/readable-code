253dcad2fde0379cb710fdbd7bcabb6b24af1f84
gwen1985 <gwen.ellegeest@outlook.com>
2020-09-14 23:01:55 +0200

    fixed code

--- experimental.php
+++ experimental.php
@@ -1,83 +1,51 @@
 <?php
-
-
-// fw = for who
-function ordr_pz($pizzatype, $fw)
+function pizzaOrder($pizzaType, $Customer)
 {
+    $Customer = ucfirst($Customer);
+    $Address = getAddress($Customer);
+    $Price = getPrice($pizzaType);
 
-    $type = $pizzatype;
-    echo 'Creating new order... <br>';
-    $toPrint = 'A ';
-    $toPrint .= $pizzatype;
-    $p = calc_cts($type);
-
-    $address = 'unknown';
-    if ($fw == 'koen') {
-        $address = 'a yacht in Antwerp';
-    } elseif ($fw == 'manuele') {
-        $address = 'somewhere in Belgium';
-    } elseif ($fw == 'students') {
-        $address = 'BeCode office';
-    }
-
-    $toPrint .= ' pizza should be sent to ' . $fw . ". <br>The address: {$address}.";
-    echo $toPrint;
-    echo '<br>';
-    echo 'The bill is €' . $p . '.<br>';
-
-
+    echo 'Creating new order...<br>';
+    echo "A {$pizzaType} pizza should be sent to {$Customer}.<br>";
+    echo "The address: {$Address}.<br>";
+    echo "The bill is €{$Price}.<br>";
     echo "Order finished.<br><br>";
 }
 
-function total_price($p)
+function getAddress($Customer)
 {
-    return $p;
-}
-
-function test($p_type)
-{
-    echo "Test: type is {$p_type}. <br>";
-}
-
-function calc_cts($p_type)
-{
-    $cst = 'unknown';
-
-    if ($p_type == 'marguerita') {
-        $cst = 5;
-    } else {
-        if ($p_type == 'golden') {
-            $cst = 100;
-        }
-
-        if ($p_type == 'calzone') {
-            $cst = 10;
-        }
-
-        if ($p_type == 'hawai') {
-            throw new Exception('Computer says no');
-        }
-    }
-
-    return $cst;
-}
-
-function ordr_piz_all()
-{
-    $test = 0;
-    ordr_pz('calzone', 'koen');
-    ordr_pz('marguerita', 'manuele');
-
-    ordr_pz('golden', 'students');
-}
-
-function make_Allhappy($do_it)
-{
-    if ($do_it) {
-        ordr_piz_all();
-    } else {
-        // Should not do anything when false
+    switch ($Customer) {
+        case 'Koen':
+            return 'Somewhere in Antwerpen';
+        case 'Manuele':
+            return 'somewhere in Belgium';
+        case 'Students':
+            return 'BeCode office';
+        default:
+            return 'N/A';
     }
 }
 
-make_Allhappy(true);
+function getPrice($pizzaType)
+{
+    switch ($pizzaType) {
+        case 'marguerita':
+            return '5';
+        case 'golden':
+            return '100';
+        case 'calzone':
+            return '10';
+        case 'hawaii':
+            throw new Exception('No way!!');
+        default:
+            return 'Comming Soon!!';
+    }
+}
+
+function main() {
+    pizzaOrder('calzone', 'koen');
+    pizzaOrder('marguerita', 'manuele');
+    pizzaOrder('golden', 'students');
+};
+
+main();
